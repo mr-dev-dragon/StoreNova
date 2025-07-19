f477c8fad74130c0dbc51b18c732fc0248a31b33
mrdevdragon <mrdevdragon@gmail.com>
2025-07-19 04:13:34 -0400

    add a demp insad the navigation to take it as a ready component

---
+++ frontend/src/Components/Navigation/NavDemo.jsx
@@ -0,0 +1,33 @@
+import Products from "../Components/Products/Products";
+import Recommended from "../Components/Recommended/Recommended";
+import { FaDev } from "react-icons/fa";
+
+import useNavigation from "../Components/Navigation/hook/useNavigation";
+import navigationData from "../Components/Navigation/data/navigation";
+import Navbar from "../Components/Navigation/navbar";
+import Tabbar from "../Components/Navigation/Tabbar";
+
+const NavDemo = () => {
+  const { currentRoute, setCurrentRoute } = useNavigation();
+
+  return (
+    <div className={"bg-gray-200 h-screen;"}>
+      <Navbar
+        navigationData={navigationData}
+        currentRoute={currentRoute}
+        setCurrentRoute={setCurrentRoute}
+      />
+      <Tabbar
+        navigationData={navigationData}
+        currentRoute={currentRoute}
+        setCurrentRoute={setCurrentRoute}
+      />
+      <div className={"flex   text-5xl text-gray-300 "}>
+        <Products />
+        <Recommended />
+      </div>
+    </div>
+  );
+};
+
+export default NavDemo;
